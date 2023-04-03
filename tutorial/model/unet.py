f5c7ae5266d1bb130a0387bcf4457baef49f556c
audreyeternal <yuzho@kth.se>
2023-04-03 16:12:49 +0200

    Refactor: Complete #11.

--- tutorial/model/unet.py
+++ tutorial/model/unet.py
@@ -60,9 +60,8 @@ class Up(nn.Module):
 
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2))
+        # x1 = torch.nn.ConstantPad2d((diffX // 2, diffX - diffX // 2,
+        #                               diffY // 2, diffY - diffY // 2),0)(x1)
         x = torch.cat([x2, x1], dim=1)
         x = self.conv(x)
         return x
