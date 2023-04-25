8a03f757a290c8518d62f6d734081cfa71a56da3
audreyeternal <yuzho@kth.se>
2023-04-25 14:49:58 +0200

    chore: publish to pypi, fix #20,#12

--- efficientbioai/__init__.py
+++ efficientbioai/__init__.py
@@ -1,1 +1,14 @@
+"""Top-level package for efficientbioai acceleration."""
+
+__author__ = "mmv_lab team"
+__email__ = "yu.zhou@isas.de"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+
+def get_module_version():
+    return __version__
+
+
 from .compress_ppl import Pipeline  # noqa: F401
