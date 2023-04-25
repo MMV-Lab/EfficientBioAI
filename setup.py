4838c70ef021a24757881fbcf63db9da5348a7ba
audreyeternal <yuzho@kth.se>
2023-04-25 15:00:00 +0200

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name="efficientbioai",
     keywords="deep learning, quantization, microscopy model compression",
     description="efficientbioai is a python package for efficient deep learning in bioimaging",
-    version="0.0.0",
+    version="0.0.1",
     author="mmv_lab team",
     author_email="yu.zhou@isas.de",
     packages=find_packages(),
