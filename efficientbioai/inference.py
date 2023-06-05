198b8d11ee554adc828101f484872590e9fa2994
audreyeternal <yuzho@kth.se>
2023-06-05 11:56:54 +0200

    chore: Update the yaml file for mmv_im2im model.

--- efficientbioai/inference.py
+++ efficientbioai/inference.py
@@ -24,11 +24,17 @@ infer_dict = dict(mmv_im2im=Mmv_im2imInfer, omnipose=OmniposeInfer)
 inference = infer_dict[model_name](config_yml=cfg_yml)
 
 # for mmv_im2im:
-# inference.run_infer()
-# inference.evaluate('./data/mmv_im2im/labelfree/pred','./data/mmv_im2im/labelfree/holdout','.tif','_GT.tiff',['SSIM','Pearson']) #currently only for mmv_im2im # noqa: E501
+inference.run_infer()
+inference.evaluate(
+    "/home/ISAS.DE/yu.zhou/EfficientBioAI/data/mmv_im2im/denoise/test/pred",
+    "/home/ISAS.DE/yu.zhou/EfficientBioAI/data/mmv_im2im/denoise/test/gt",
+    ".tif",
+    ".tif",
+    ["Dice"],
+)  # currently only for mmv_im2im # noqa: E501
 # inference.calculate_infer_time(num= 100)
 # inference.calculate_energy(num=100)
-inference.calculate_energy_latency(num=5)
+# inference.calculate_energy_latency(num=100, image_size=[95, 1024, 1024])
 
 # for omnipose:
 # inference.run_infer()
